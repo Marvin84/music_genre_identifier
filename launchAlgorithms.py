eb5f2ba86436eb359bd7746f0983d4de9881a669
Marvin84 <tinaraissi@gmail.com>
2016-10-30 18:05:01 +0100

    minor changes

--- launchAlgorithms.py
+++ launchAlgorithms.py
@@ -9,15 +9,7 @@ import mySeed
 def launch_lagrange(training, test, percentageLabel, r):
     xtrain_l, ytrain_l, xtrain_u, xtest, ytest, l, u = get_lagrange_dataset(training, test, [1, 3], percentageLabel, True)
     best_estimator = get_best_estimator_by_cv(xtrain_l, ytrain_l, 3)
-    s3vc = lagrangian_s3vm_train(l,
-                                 u,
-                                 xtrain_l,
-                                 ytrain_l,
-                                 xtrain_u,
-                                 C=best_estimator.C,
-                                 gamma=best_estimator.gamma,
-                                 kernel=best_estimator.kernel,
-                                 r=r,
-                                 rdm = mySeed.rdm
-                                 )
-    return s3vc.score(xtest,ytest)
+    model = lagrangian_s3vm_train(l, u, xtrain_l, ytrain_l, xtrain_u, C=best_estimator.C,
+                                 gamma=best_estimator.gamma, kernel=best_estimator.kernel, r=r, rdm = mySeed.rdm)
+
+    return model.score(xtest,ytest)
